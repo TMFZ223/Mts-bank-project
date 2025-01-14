af20f78deba2ad98cf47959606f7b3de385772e1
Ftemur <153289154+TMFZ223@users.noreply.github.com>
2025-01-14 14:45:42 +0300

    Update Preconditions.java

--- src/test/java/Mts/Preconditions.java
+++ src/test/java/Mts/Preconditions.java
@@ -1,6 +1,7 @@
 package Mts;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
 import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selenide.open;
@@ -14,8 +15,11 @@ private long timeout = 40000; // ожидание для элементов в �
     public  void openChrome() {
         WebDriverManager.chromedriver().setup();
         browser = "chrome";
-        Configuration.browserSize = "1920x1080"; // открытие браузера в полноэкранном режиме
+        Configuration.webdriverLogsEnabled = true;
+        Configuration.headless = true;
+                Configuration.browserSize = "1920x1080"; // открытие браузера в полноэкранном режиме
         Configuration.timeout = timeout; //
+        System.setProperty("chromeoptions.args", "\"--no-sandbox\",\"--disable-dev-shm-usage\",\"--remote-debugging-port=9222\",\"--headless\"");
     }
     @Step("Перейти на страницу: {this.mainPageUrl}")
     public void goToUrl() {
