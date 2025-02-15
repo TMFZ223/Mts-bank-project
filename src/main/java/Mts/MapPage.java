3c7a9a2a104fe5e0b2ade390c63b268b3320da4d
Ftemur <153289154+TMFZ223@users.noreply.github.com>
2025-02-15 23:38:26 +0300

    Update MapPage.java

--- src/main/java/Mts/MapPage.java
+++ src/main/java/Mts/MapPage.java
@@ -2,6 +2,7 @@ package Mts;
 
 import com.codeborne.selenide.*;
 import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
@@ -9,16 +10,17 @@ public class MapPage {
     private SelenideElement filtersButton = $x("//div[@class='styled__MainRow-sc-c547a2c6-1 bmrlcA']"); // Элемент фильтров для объектов на карте
     private ElementsCollection filtersList = $$x("//label[input[@name='defaultName']]//div[@type='checkbox']"); // список фильтров
     private SelenideElement listButton = $x("//button[@class='TabItem-sc-284dbe6b-4 kuAyes']"); // Кнопка показа списком
-    private  ElementsCollection objects = $$x("//div[@class='styled__MainDataTextContainer-sc-d623ef82-2 coNzXP']"); // элементы, показывающие тип объекта
+    private ElementsCollection objects = $$x("//div[@class='styled__MainDataTextContainer-sc-d623ef82-2 coNzXP']"); // элементы, показывающие тип объекта
+    private SelenideElement emptyObjectElement = $x("//div[@class='Wrapper-sc-6189bc63-0 lgYRtB']"); // Элемент для не найденных объектов
 
     @Step("Кликнуть по фильтрам")
-    public  void clickFiltersButton() {
+    public void clickFiltersButton() {
         filtersButton.scrollIntoView("{block: 'center'}");
         filtersButton.shouldBe(Condition.visible).click();
     }
 
     @Step("Обработать необходимые флажки с фильтрами")
-    public  void  changeFilters(int... indices) {
+    public void changeFilters(int... indices) {
         for (int index : indices) {
             if (index >= 0 && index < filtersList.size()) {
                 filtersList.get(index).click(); // Отмечаем флажок по индексу
@@ -30,16 +32,22 @@ public class MapPage {
     }
 
     @Step("Кликнуть на кнопку для отображения списком")
-    public  void clickListButton() {
+    public void clickListButton() {
         listButton.scrollIntoView("{block: 'center'}");
         listButton.shouldBe(Condition.visible).click();
     }
-    @Step("Убедиться, что отображается тип объекта {type}")
-    public  void checkTypeObject(String type) {
-objects.shouldBe(CollectionCondition.sizeGreaterThan(0));
-for (SelenideElement element: objects) {
-    element.scrollIntoView("{block: 'center'}");
-    element.shouldHave(Condition.partialText(type));
-}
+
+    @Step("Убедиться, что отображается текст {emptyObjectElementText} или тип объекта {type}")
+    public void checkTypeObject(String emptyObjectElementText, String type) {
+        if (emptyObjectElement.exists()) {
+            emptyObjectElement.scrollIntoView("{block: 'center'}");
+            emptyObjectElement.shouldHave(Condition.text(emptyObjectElementText));
+        } else {
+            objects.shouldBe(CollectionCondition.sizeGreaterThan(0));
+            for (SelenideElement element : objects) {
+                element.scrollIntoView("{block: 'center'}");
+                element.shouldHave(Condition.partialText(type));
+            }
+        }
     }
 }
